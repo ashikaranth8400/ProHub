7591dccfecf7a501211ec7ce334c639ae90567fe
Ashika K <42943409+ashikaranth8400@users.noreply.github.com>
2022-05-29 13:17:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express =require('express');
+const App =express();
+const connectDB=require('./config/db');
+const path=require('path');
+
+
+connectDB();
+
+App.use(express.json({extended:false}))
+
+
+//define Routes
+App.use('/api/users',require('./routes/api/users'));
+App.use('/api/profile',require('./routes/api/profile'));
+App.use('/api/posts',require('./routes/api/posts'));
+App.use('/api/auth',require('./routes/api/auth'));
+
+if(process.env.NODE_ENV==='production')
+{
+    App.use(express.static('client/build'));
+
+    App.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      });
+    
+    
+}
+
+const PORT= process.env.PORT ||5000;
+App.listen(PORT,()=>console.log(`server started on ${PORT}`))
